eb4e356441b3dc25e75b65a1221180d39a8ffbd6
lpavezb <lukas.pavez@ug.uchile.cl>
2019-06-19 02:20:16 -0400

    :cat:working on feature selection:cat:

--- proyecto/proyect.py
+++ proyecto/proyect.py
@@ -1,61 +1,43 @@
-from time import time
-import numpy as np
-from matplotlib import pyplot as plt
-import pandas as pd
 import os
-from sklearn.model_selection import GridSearchCV
-from sklearn import svm
-from sklearn import metrics
+import numpy as np
+import pandas as pd
 import tensorflow as tf
-from sklearn.preprocessing import LabelBinarizer
+from time import time
+from matplotlib import pyplot as plt
+from sklearn import svm, metrics
+from sklearn.model_selection import GridSearchCV
+from sklearn.preprocessing import LabelBinarizer, StandardScaler
+from sklearn.feature_selection import SelectKBest, mutual_info_classif
+
+import math
+from scipy.stats import kurtosis, skew, entropy
 
 
-def plot_data(subject):
-    s1 = load_data([subject], path, empty_data)
-    plt.figure()
-    plt.suptitle("subject {}".format(subject), fontsize="x-large")
-    plt.subplot(5, 1, 1)
-    plt.plot(range(len(s1["class"])), s1["class"])
-    for i in range(8):
-        plt.subplot(5, 2, i + 3)
-        c = "channel{}".format(i + 1)
-        plt.plot(range(len(s1)), s1[c])
-        plt.title(c)
-    plt.subplots_adjust(hspace=1, wspace=0.35)
+def average_entropy(data):
+    return entropy(data)/len(data)
 
 
-def load_data(subjects, path, data, drop7=True):
-    ndata = data.copy()
-    for subject in subjects:
-        new_path = path + subject + "/"
-        d1, d2 = os.listdir(new_path)
-        ndata = ndata.append(pd.read_csv(new_path + d1, sep="\t"))
-        ndata = ndata.append(pd.read_csv(new_path + d2, sep="\t"))
-    if drop7:
-        ndata = ndata[ndata["class"] != 7]
-    return ndata
+def root_mean_square(data):
+    square = 0
+    for i in range(len(data)):
+        square += (data[i] ** 2)
+
+    root = math.sqrt(square/len(data))
+    return root
 
 
-def windows(data, width, step):
-    start = 0
-    res = []
-    while (start + width) < len(data):
-        data_slice = data[start:start+width].reset_index()
-        if data_slice["class"][0] == data_slice["class"][199]:
-            res.append(data_slice)
-        start += width
-        start += step
-    return res
-
-
-def get_features(window):
-    features = {}
-    for i in range(1, 9):
-        features["mean"+ str(i)] = np.mean(window["channel" + str(i)])
-        features["min" + str(i)] = np.min(window["channel" + str(i)])
-        features["max" + str(i)] = np.max(window["channel" + str(i)])
-    features["label"] = window["class"][0]
-    return features
+# def plot_data(subject):
+#     s1 = load_data([subject], path, empty_data)
+#     plt.figure()
+#     plt.suptitle("subject {}".format(subject), fontsize="x-large")
+#     plt.subplot(5, 1, 1)
+#     plt.plot(range(len(s1["class"])), s1["class"])
+#     for i in range(8):
+#         plt.subplot(5, 2, i + 3)
+#         c = "channel{}".format(i + 1)
+#         plt.plot(range(len(s1)), s1[c])
+#         plt.title(c)
+#     plt.subplots_adjust(hspace=1, wspace=0.35)
 
 
 def perceptron_classifier(train, valid):
@@ -144,10 +126,10 @@ def perceptron_classifier(train, valid):
         print("----------------------------------------------")
 
 
-def smv_classifier(train, valid):
+def svm_classifier(train, valid):
     x_train, y_train = train.drop("label", 1), train["label"]
     x_data, y_data = valid.drop("label", 1), valid["label"]
-    svc = svm.LinearSVC(multi_class="crammer_singer")
+    svc = svm.LinearSVC()
     t1 = time()
     parameters = {'C': [1, 10, 100, 1000]}
     grid = GridSearchCV(svc, parameters, cv=5)
@@ -155,11 +137,59 @@ def smv_classifier(train, valid):
 
     classifier = grid.best_estimator_
     predictions = classifier.predict(x_data)
-    print("-----------------------------------------------")
-    print(metrics.confusion_matrix(y_data, predictions))
+    confm = metrics.confusion_matrix(y_data, predictions)
+    confm_diagonal = np.diag(confm)
     t2 = time()
+    print("-----------------------------------------------")
+    print(confm)
+    print("classifier: {}".format(classifier))
+    print("Accuracy: {:.4f}".format(confm_diagonal.sum() / confm.sum()))
     print("training time = {:.2f}".format(t2 - t1))
     print("-----------------------------------------------")
+
+
+def load_data(subjects, path, data, drop7=True):
+    ndata = data.copy()
+    for subject in subjects:
+        new_path = path + subject + "/"
+        d1, d2 = os.listdir(new_path)
+        ndata = ndata.append(pd.read_csv(new_path + d1, sep="\t"), sort=True)
+        ndata = ndata.append(pd.read_csv(new_path + d2, sep="\t"), sort=True)
+    if drop7:
+        ndata = ndata[ndata["class"] != 7]
+    ndata = ndata.drop("time", 1)
+    return ndata.drop("class", 1), ndata["class"]
+
+
+def windows_without_zeros(data, width, step):
+    start = 0
+    res = []
+    while (start + width) < len(data):
+        data_slice = data[start:start+width].reset_index()
+        if (data_slice["class"][0] == data_slice["class"][199]) and data_slice["class"][0] != 0:
+            res.append(data_slice)
+        start += width
+        start += step
+    return res
+
+
+def get_features(window):
+    features = {}
+    for i in range(1, 9):
+        max_value = np.min(window["channel" + str(i)])
+        min_value = np.max(window["channel" + str(i)])
+        features["mean"+ str(i)] = np.mean(window["channel" + str(i)])
+        features["variance" + str(i)] = np.var(window["channel" + str(i)])
+        features["min" + str(i)] = min_value
+        features["max" + str(i)] = max_value
+        features["range" + str(i)] = max_value - min_value
+        features["RMS" + str(i)] = root_mean_square(window["channel" + str(i)])
+        features["skew" + str(i)] = skew(window["channel" + str(i)])
+        # features["AvEnt" + str(i)] = average_entropy(window["channel" + str(i)])
+        features["kurtosis" + str(i)] = kurtosis(window["channel" + str(i)])
+
+    features["label"] = window["class"][0]
+    return features
 
 
 if __name__ == "__main__":
@@ -172,37 +202,82 @@ if __name__ == "__main__":
     t_len = int(len(train_validation)*0.6)
 
     test_subjects = subjects[test_len:]
-    train_subjects = train_validation[:t_len]
-    validation_subjects = train_validation[t_len:]
-    print(train_subjects)
-    empty_data = pd.DataFrame(columns=["time", "channel1", "channel2", "channel3", "channel4", "channel5", "channel6", "channel7", "channel8", "class"])
-    train = load_data(train_subjects, path, empty_data)
-    valid = load_data(validation_subjects, path, empty_data)
+    # train_subjects = train_validation[:t_len]
+    # validation_subjects = train_validation[t_len:]
+
+    train_subjects = ["01", "02", "03", "04"]
+    validation_subjects = ["06", "07", "08", "09"]
+    columns = ["channel1", "channel2", "channel3", "channel4", "channel5", "channel6", "channel7", "channel8", "class"]
+    empty_data = pd.DataFrame(columns=columns)
+
+    print(100 * '-')
+    print(44 * '-' + "loading data" + 44 * '-')
+    t = time()
+    x_train, y_train = load_data(train_subjects, path, empty_data)
+    x_valid, y_valid = load_data(validation_subjects, path, empty_data)
     # test = load_data(test_subjects, path, empty_data)
-    train_windows = windows(train, 200, 200)
-    valid_windows = windows(valid, 200, 200)
-    print("train: {}".format(len(train)))
+
+    print("train_samples: {}".format(len(x_train)))
+    print("validation_samples: {}".format(len(x_valid)))
+    print("time: {:.2f}".format(time() - t))
+    print(100 * '-')
+
+    print(36 * '-' + "Standardization of datasets" + 37 * '-')
+    t = time()
+    scaler = StandardScaler()
+    scaler.fit(x_train)
+    x_train = scaler.transform(x_train)
+    x_valid = scaler.transform(x_valid)
+    print("time: {:.2f}".format(time() - t))
+    print(100 * '-')
+
+    print(40 * '-' + "calculating windows" + 41 * '-')
+    t = time()
+    t_values = np.hstack((x_train, y_train.to_numpy().reshape(len(x_train), 1)))
+    v_values = np.hstack((x_valid, y_valid.to_numpy().reshape(len(x_valid), 1)))
+    train = pd.DataFrame(columns=columns, data=t_values)
+    valid = pd.DataFrame(columns=columns, data=v_values)
+
+    train_windows = windows_without_zeros(train, 200, 200)
+    valid_windows = windows_without_zeros(valid, 200, 200)
+
     print("train_windows: {}".format(len(train_windows)))
+    print("validation_windows: {}".format(len(valid_windows)))
+    print("time: {:.2f}".format(time() - t))
+    print(100 * '-')
+
+    print(40 * '-' + "calculating features" + 40 * '-')
+    t = time()
+    # features = ["mean", "variance", "min", "max", "range", "RMS", "skew", "AvEnt", "kurtosis"]
+    features = ["mean", "variance", "min", "max", "range", "RMS", "skew", "kurtosis"]
+    for feature in features:
+        print(feature)
     cols = []
     for i in range(1, 9):
-        cols.append("mean"+ str(i))
-        cols.append("min" + str(i))
-        cols.append("max" + str(i))
+        for feature in features:
+            cols.append(feature + str(i))
     cols.append("label")
+
     t_windows = pd.DataFrame(columns=cols)
     for window in train_windows:
         window_features = get_features(window)
         t_windows = t_windows.append(window_features, ignore_index=True)
-    print("train_windows features: {}".format(len(t_windows)))
     v_windows = pd.DataFrame(columns=cols)
     for window in train_windows:
         window_features = get_features(window)
         v_windows = v_windows.append(window_features, ignore_index=True)
-    # t_windows = t_windows[t_windows["label"] != 0]
-    # v_windows = v_windows[v_windows["label"] != 0]
-    print("-----------------------------------------------")
-    print("---------------value counts--------------------")
+    print("time: {:.2f}".format(time() - t))
+    print(100 * '-')
+
+    print(44 * '-' + "value counts" + 44 * '-')
     print(t_windows["label"].value_counts())
-    print("-----------------------------------------------")
-    print("train_windows features (0 removed): {}".format(len(t_windows)))
-    perceptron_classifier(t_windows, v_windows)
+    print(len(t_windows))
+    print(100 * "-")
+
+    print(41 * '-' + "feature selection" + 42 * '-')
+    X, y = t_windows.drop("label", 1).to_numpy(), t_windows["label"].to_numpy()
+    print(X[0])
+    X_new = SelectKBest(mutual_info_classif, k=15).fit_transform(X, y)
+    print(X_new)
+    print(100 * "-")
+    #svm_classifier(t_windows, v_windows)
